d0b1d849e547856dfa3790c3eeac7a212ca6c597
synywildan2405 <68019382+synywildan2405@users.noreply.github.com>
2020-07-30 21:14:29 +0700

    Add files via upload

---
+++ deteksiWarna.py
@@ -0,0 +1,23 @@
+#Nama   : Wildan Taufik H (18041009)
+#Kelas  : 4G
+#MK     : Computer Vision
+#Program Deteksi Warna
+
+import cv2
+import numpy as np
+
+img = cv2.imread('apel.jpg', 1)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+lower_range = np.array([100, 100, 100], dtype=np.uint8)
+upper_range = np.array([255, 180, 180], dtype=np.uint8)
+mask = cv2.inRange(hsv, lower_range, upper_range)
+
+cv2.imshow('mask', mask)
+cv2.imshow('image', img)
+
+while (1):
+    k = cv2.waitKey(0)
+    if (k == 27):
+        break
+
+cv2.destroyAllWindows()
